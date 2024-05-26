b9aaea807a5952b5be297304d9803a4519b8cdc0
TermProject <jtyk119@naver.com>
2024-05-26 19:34:06 +0900

    버튼 메인으로 변경

--- data.py
+++ data.py
@@ -1,59 +1,31 @@
 import requests
 import xml.etree.ElementTree as ET
 from tkinter import *
-from tkinter import font
-#병원정보 서비스 예제
-window = Tk()
-window.title("쉼터정보")
-window.geometry("700x500")
 
-url = 'https://openapi.gg.go.kr/Heatwaverestarere?KEY=47bd6dc9f9724cc787a3164da514a319&pIndex=1&pSize1000'
+url = 'https://openapi.gg.go.kr/Heatwaverestarere?KEY=47bd6dc9f9724cc787a3164da514a319&pIndex=1&pSize=1000'
 
-def process():
-    dollar = NameEntry.get()
-    queryParams['SIGUN_NM'] = dollar
+def process(str, listbox):
+    queryParams= {'SIGUN_NM': str}
 
     response = requests.get(url, params=queryParams)
     root = ET.fromstring(response.text)
-    ShelterListBox.delete(0, END)
+    listbox.delete(0, END)
 
     row_count = 1
     for item in root.iter("row"):
         faclt = item.findtext("RESTARER_FACLT_NM")   # 쉼터명
-        typediv = item.findtext("RESTARER_TYPE_DIV_NM")  # 시설유형
-        area = item.findtext("FACLT_AR")   # 면적 (m^2)
-        pncnt = item.findtext("UTLZ_POSBL_PSNNUM_CNT")  # 이용가능인원수
-        elefancnt = item.findtext("ELEFAN_HOLD_CNT")   # 선풍기보유현황
-        arcndtncnt = item.findtext("ARCNDTN_HOLD_CNT")  # 에어컨보유현황
-        night = item.findtext("NIGHT_EXTS_OPERT_YN")  # 야간개방
-        wkend = item.findtext("WKEND_OPERT_YN")   # 휴일개방
-        syayng = item.findtext("STAYNG_POSBL_YN ")  # 숙박가능여부
-        partclr = item.findtext("PARTCLR_MATR")  # 특이사항
-        telno = item.findtext("MNGINST_TELNO")  # 관리기관전화번호
-        latitude = item.findtext("REFINE_WGS84_LAT")  # 위도
-        hardness = item.findtext("REFINE_WGS84_LOGT")  # 경도
+        # typediv = item.findtext("RESTARER_TYPE_DIV_NM")  # 시설유형
+        # area = item.findtext("FACLT_AR")   # 면적 (m^2)
+        # pncnt = item.findtext("UTLZ_POSBL_PSNNUM_CNT")  # 이용가능인원수
+        # elefancnt = item.findtext("ELEFAN_HOLD_CNT")   # 선풍기보유현황
+        # arcndtncnt = item.findtext("ARCNDTN_HOLD_CNT")  # 에어컨보유현황
+        # night = item.findtext("NIGHT_EXTS_OPERT_YN")  # 야간개방
+        # wkend = item.findtext("WKEND_OPERT_YN")   # 휴일개방
+        # syayng = item.findtext("STAYNG_POSBL_YN ")  # 숙박가능여부
+        # partclr = item.findtext("PARTCLR_MATR")  # 특이사항
+        # telno = item.findtext("MNGINST_TELNO")  # 관리기관전화번호
+        # latitude = item.findtext("REFINE_WGS84_LAT")  # 위도
+        # hardness = item.findtext("REFINE_WGS84_LOGT")  # 경도
 
-
-        ShelterListBox.insert(row_count, faclt)
+        listbox.insert(row_count, faclt)
         row_count += 1
-
-NameEntry = Entry(window)
-NameEntry.pack()
-NameEntry.place(x=10, y=80)
-
-queryParams = {}
-b1 = Button(window, text='검색', command=process)
-b1.place(x=160, y= 80)
-
-
-frame = Frame(window)
-frame.pack()
-
-TempFont = font.Font(window, size=10, family='Consolas')
-ShelterListBox = Listbox(window, font=TempFont, activestyle='none',
-                         width=40, height=22, borderwidth=3, relief='ridge')
-
-ShelterListBox.pack()
-ShelterListBox.place(x=10, y=110)
-
-window.mainloop()
--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 from tkinter import *
+from tkinter import font
 import tkinter.ttk
+from data import *
 
 class main:
-    window = Tk(className="쉼터")
-    notebook = tkinter.ttk.Notebook(window, width=800, height=600)
-    notebook.pack()
+    def __init__(self):
+        self.window = Tk(className="쉼터")
+        self.notebook = tkinter.ttk.Notebook(self.window, width=700, height=500)
+        self.notebook.pack()
+        frame1 = self.frame1()
+        self.notebook.add(frame1, text="메인")
+        frame2 = self.frame2()
+        self.notebook.add(frame2, text="쉼터 정보")
+        frame3 = self.frame3()
+        self.notebook.add(frame3, text="즐겨찾기")
+        self.window.mainloop()
 
-    frame1 = Frame(window)
-    notebook.add(frame1, text="메인")
-    Label(frame1, text="메인 페이지", font='helvetica 48').pack()
+    def frame1(self):
+        frame1 = Frame(self.window)
+        self.notebook.add(frame1, text="메인")
+        title = Label(frame1, text="쉼터", font='helvetica 48')
+        title.place(x=10, y=10)
 
-    frame2 = Frame(window)
-    notebook.add(frame2, text="쉼터 정보")
-    Label(frame2, text="쉼터 정보 페이지", font='helvetica 48').pack()
+        NameEntry = Entry(frame1)
+        NameEntry.place(x=10, y=90)
 
-    frame3 = Frame(window)
-    notebook.add(frame3, text="즐겨찾기")
-    Label(frame3, text="즐겨찾기 페이지", font='helvetica 48').pack()
+        TempFont = font.Font(self.window, size=10, family='Consolas')
+        ShelterListBox = Listbox(frame1, font=TempFont, activestyle='none',
+                                 width=40, height=22, borderwidth=3, relief='ridge')
+        ShelterListBox.place(x=10, y=120)
 
-    window.mainloop()
+        b1 = Button(frame1, text='검색', command=lambda:process(NameEntry.get(), ShelterListBox))
+        b1.place(x=160, y=90)
+
+        return frame1
+
+    def frame2(self):
+        frame2 = Frame(self.window)
+        Label(frame2, text="쉼터 정보 페이지", font='helvetica 48').pack()
+        return frame2
+
+    def frame3(self):
+        frame3 = Frame(self.window)
+        Label(frame3, text="즐겨찾기 페이지", font='helvetica 48').pack()
+        return frame3
+
+
 
 main()
