f4f904252e7b767425dd29b538dd73652b46cb9b
TermProject <jtyk119@naver.com>
2024-06-06 14:00:24 +0900

    선택한 쉼터 정보 보내기

--- data.py
+++ data.py
@@ -97,17 +97,47 @@ def processMaps(str, listbox, map_widget):
             break
 
 
-def processGmail():
+def processGmail(str, listbox):
+    queryParams = {'SIGUN_NM': str}
+
+    response = requests.get(url, params=queryParams)
+    root = ET.fromstring(response.text)
+
+    selected_indices = listbox.curselection()
+    if not selected_indices:
+        return
+    shelters_data = []
+    selected_faclt = listbox.get(selected_indices[0])
+    for item in root.iter("row"):
+        if item.findtext("RESTARER_FACLT_NM") == selected_faclt:
+            shelter = {
+                "faclt": item.findtext("RESTARER_FACLT_NM"),  # 쉼터명
+                "typediv": item.findtext("RESTARER_TYPE_DIV_NM"),  # 시설유형
+                "area": item.findtext("FACLT_AR"),  # 면적 (m^2)
+                "pncnt": item.findtext("UTLZ_POSBL_PSNNUM_CNT"),  # 이용가능인원수
+                "elefancnt": item.findtext("ELEFAN_HOLD_CNT"),  # 선풍기보유현황
+                "arcndtncnt": item.findtext("ARCNDTN_HOLD_CNT"),  # 에어컨보유현황
+                "night": item.findtext("NIGHT_EXTS_OPERT_YN"),  # 야간개방
+                "wkend": item.findtext("WKEND_OPERT_YN"),  # 휴일개방
+                "syayng": item.findtext("STAYNG_POSBL_YN "),  # 숙박가능여부
+                "partclr": item.findtext("PARTCLR_MATR"),  # 특이사항
+                "telno": item.findtext("MNGINST_TELNO"),  # 관리기관전화번호
+                "Lmna": item.findtext("REFINE_ROADNM_ADDR"),  # 소재지도로명주소
+                "Lna": item.findtext("REFINE_LOTNO_ADDR"),  # 소재지지번주소
+                "ZipCode": item.findtext("REFINE_ZIP_CD")  # 우편번호
+            }
+            shelters_data.append(shelter)
+
     window = Tk(className="받을 메일 입력")
     window.geometry("250x70")
     recipientEntry = Entry(window, width=230)
     recipientEntry.place(x=0, y=10)
-    recipientButton = Button(window, text="    전송    ", command=lambda:mailsend(recipientEntry))
+    recipientButton = Button(window, text="    전송    ", command=lambda:mailsend(recipientEntry, shelters_data))
     recipientButton.place(x=90, y=36)
     window.mainloop()
 
 
-def mailsend(recipientEntry):
+def mailsend(recipientEntry, shelters_data):
     host = "smtp.gmail.com"
     port = "587"
     title = "쉼터 정보"
@@ -124,7 +154,22 @@ def mailsend(recipientEntry):
     msg['From'] = senderAddr
     msg['To'] = recipientAddr
 
-    msgtext = "쉼터 정보입니다."
+    msgtext = ""
+    for shelter in shelters_data:
+        msgtext += (f"시설명 : {shelter['faclt']}\n"
+                    f"시설유형 : {shelter['typediv']}\n"
+                    f"면적(m²) : {shelter['area']}\n"
+                    f"이용가능인원수 : {shelter['pncnt']}\n"
+                    f"선풍기보유현황 : {shelter['elefancnt']}\n"
+                    f"에어컨보유현황 : {shelter['arcndtncnt']}\n"
+                    f"야간개방 : {shelter['night']}\n"
+                    f"휴일개방 : {shelter['wkend']}\n"
+                    f"숙박가능여부 : {shelter['syayng']}\n"
+                    f"특이사항 : {shelter['partclr']}\n"
+                    f"관리기관전화번호 : {shelter['telno']}\n"
+                    f"소재지도로명주소 : {shelter['Lmna']}\n"
+                    f"소재지지번주소 : {shelter['Lna']}\n"
+                    f"우편번호 : {shelter['ZipCode']}\n\n")
     msgPart = MIMEText(msgtext, 'plain')
     msg.attach(msgPart)
 
